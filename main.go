7c5ac9ad26716cd938e908170339495daf3a12bc
jimmy <github@jimmysawczuk.com>
2013-03-26 10:52:22 -0400

    Made scheduling more intelligent.

--- main.go
+++ main.go
@@ -7,37 +7,59 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"time"
 )
 
 var pathToLESS string
 var workingDirectory string
 
+type Job struct {
+	name        string
+	cmd         *exec.Cmd
+	cmd_min     *exec.Cmd
+	css_out     string
+	css_min_out string
+}
+
+type Worker struct {
+	jobs    []*Job
+	started bool
+
+	max_jobs     int
+	running_jobs int
+}
+
+var jobs_queue Worker
+
 func main() {
 	var err error
 	workingDirectory, err = os.Getwd()
 	if err != nil {
-		log.Fatalf("Can't find the working directory. Massive fail.")
+		log.Fatalf("Can't find the working directory.")
 		os.Exit(1)
 		return
 	}
 
 	flag.StringVar(&pathToLESS, "path", "lessc", "Path to the lessc executable")
-
 	flag.Parse()
 
+	jobs_queue = NewWorker()
+
 	args := flag.Args()
-	chans := make([]chan int, len(args))
-	for i, v := range args {
-		chans[i] = make(chan int)
-		go compileFromRoot(v, chans[i])
+	for _, v := range args {
+		compileFromRoot(v)
 	}
 
-	for i, _ := range chans {
-		<-chans[i]
+	running_jobs_chan := make(chan int)
+	running_jobs := 1
+	for running_jobs > 0 {
+		go jobs_queue.Start(running_jobs_chan)
+		running_jobs = <-running_jobs_chan
 	}
+
 }
 
-func compileFromRoot(dir string, ch chan int) {
+func compileFromRoot(dir string) {
 	var fq_dir *os.File
 	var err error
 
@@ -79,19 +101,14 @@ func compileFromRoot(dir string, ch chan int) {
 		}
 	}
 
-	dir_chan := make(chan int)
-	go compileDirectory("", less_dir, css_dir, dir_chan)
-	<-dir_chan
-	ch <- 1
+	compileDirectory("", less_dir, css_dir)
 }
 
-func compileDirectory(prefix string, less_dir, css_dir *os.File, ch chan int) {
+func compileDirectory(prefix string, less_dir, css_dir *os.File) {
 	files, err := less_dir.Readdir(-1)
 	if err != nil {
 		log.Panicf("Can't parse %s", less_dir.Name())
 	}
-
-	chs := make([]chan int, 0)
 
 	for _, v := range files {
 		if v.IsDir() {
@@ -110,38 +127,97 @@ func compileDirectory(prefix string, less_dir, css_dir *os.File, ch chan int) {
 				}
 			}
 
-			dir_ch := make(chan int)
-			chs = append(chs, dir_ch)
-			go compileDirectory(v.Name()+"/", less_deeper, css_deeper, dir_ch)
+			compileDirectory(v.Name()+"/", less_deeper, css_deeper)
 
 		} else if v.Name()[0:1] != "_" {
-			file_ch := make(chan int)
-			chs = append(chs, file_ch)
-			go compileFile(less_dir, css_dir, v, file_ch, prefix+v.Name())
+			compileFile(less_dir, css_dir, v, prefix+v.Name())
 		}
 	}
-
-	for i, _ := range chs {
-		<-chs[i]
-	}
-
-	ch <- 1
 }
 
-func compileFile(less_dir, css_dir *os.File, less_file os.FileInfo, ch chan int, log_text string) {
+func compileFile(less_dir, css_dir *os.File, less_file os.FileInfo, log_text string) {
 
-	fmt.Println(log_text)
+	jobs_queue.Add(Job{
+		name:        log_text,
+		cmd:         exec.Command(pathToLESS, less_dir.Name()+"/"+less_file.Name()),
+		cmd_min:     exec.Command(pathToLESS, "-x", less_dir.Name()+"/"+less_file.Name()),
+		css_out:     css_dir.Name() + "/" + strings.Replace(less_file.Name(), ".less", ".css", 1),
+		css_min_out: css_dir.Name() + "/" + strings.Replace(less_file.Name(), ".less", ".min.css", 1),
+	})
+
+}
+
+func NewWorker() Worker {
+	w := Worker{
+		started:      false,
+		max_jobs:     3,
+		running_jobs: 0,
+	}
+
+	return w
+}
+
+func (w *Worker) Add(j Job) {
+	w.jobs = append(w.jobs, &j)
+}
+
+func (w *Worker) Start(return_ch chan int) {
+	if !w.started {
+		w.started = true
+
+		for len(w.jobs) > 0 {
+			ch := make(chan bool)
+			go w.runNextJob(ch)
+
+			result := <-ch
+
+			if !result {
+				time.Sleep(100 * time.Millisecond)
+			}
+		}
+
+		w.started = false
+	}
+
+	return_ch <- w.running_jobs
+}
+
+func (w *Worker) getNextJob() *Job {
+	j := w.jobs[0]
+	w.jobs = w.jobs[1:len(w.jobs)]
+	return j
+}
+
+func (w *Worker) runNextJob(ch chan bool) {
+	var job_ch chan int
+
+	if w.running_jobs < w.max_jobs {
+		ch <- true
+
+		job := w.getNextJob()
+		job_ch = make(chan int)
+		w.running_jobs++
+		go job.Run(job_ch)
+	} else {
+		ch <- false
+	}
+
+	<-job_ch
+	w.running_jobs--
+}
+
+func (j *Job) Run(ch chan int) {
+
+	log.Printf("Compiling %s", j.name)
 
 	(func() {
-		cmd := exec.Command(pathToLESS, less_dir.Name()+"/"+less_file.Name())
-
-		result, err := cmd.Output()
+		result, err := j.cmd.Output()
 		if err != nil {
-			fmt.Println(err)
+			fmt.Errorf("Command error: %s\n", err)
 		} else {
-			dest_file, err := os.OpenFile(css_dir.Name()+"/"+strings.Replace(less_file.Name(), ".less", ".css", 1), os.O_RDWR+os.O_TRUNC+os.O_CREATE, 0644)
+			dest_file, err := os.OpenFile(j.css_out, os.O_RDWR+os.O_TRUNC+os.O_CREATE, 0644)
 			if err != nil {
-				log.Println(err)
+				fmt.Errorf("File output error: %s\n", err)
 			} else {
 				dest_file.Write(result)
 			}
@@ -149,15 +225,13 @@ func compileFile(less_dir, css_dir *os.File, less_file os.FileInfo, ch chan int,
 	})()
 
 	(func() {
-		cmd := exec.Command(pathToLESS, "-x", less_dir.Name()+"/"+less_file.Name())
-
-		result, err := cmd.Output()
+		result, err := j.cmd_min.Output()
 		if err != nil {
-			fmt.Println(err)
+			fmt.Errorf("Command error: %s\n", err)
 		} else {
-			dest_file, err := os.OpenFile(css_dir.Name()+"/"+strings.Replace(less_file.Name(), ".less", ".min.css", 1), os.O_RDWR+os.O_TRUNC+os.O_CREATE, 0644)
+			dest_file, err := os.OpenFile(j.css_min_out, os.O_RDWR+os.O_TRUNC+os.O_CREATE, 0644)
 			if err != nil {
-				log.Println(err)
+				fmt.Errorf("File output error: %s\n", err)
 			} else {
 				dest_file.Write(result)
 			}
@@ -165,4 +239,5 @@ func compileFile(less_dir, css_dir *os.File, less_file os.FileInfo, ch chan int,
 	})()
 
 	ch <- 1
+
 }
